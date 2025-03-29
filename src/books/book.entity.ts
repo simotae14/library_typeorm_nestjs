aa64329803b659d14a5de05becf4e7ee9d3dc970
Simone Taeggi <startae14@gmail.com>
2025-03-29 23:38:46 +0100

    Create Book Entity

---
+++ src/books/book.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  author: string;
+}
