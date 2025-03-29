95fdafcc9315ac8f3f51ac5a39118f9cfa2a2235
Simone Taeggi <startae14@gmail.com>
2025-03-30 00:24:28 +0100

    Get All Books and Get Book by ID implementation

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { Book } from './book.entity';
 import { CreateBookDto } from './dto/create-book.dto';
@@ -7,8 +7,18 @@ import { CreateBookDto } from './dto/create-book.dto';
 export class BooksController {
   constructor(private bookService: BooksService) {}
 
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+
   @Post()
   createBook(@Body() body: CreateBookDto): Promise<Book> {
     return this.bookService.create(body);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<Book> {
+    return this.bookService.findOne(id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { Repository } from 'typeorm';
@@ -10,6 +10,10 @@ export class BooksService {
     private repo: Repository<Book>,
   ) {}
 
+  async findAll(): Promise<Book[]> {
+    return this.repo.find();
+  }
+
   create(book: Book): Promise<Book> {
     const newBook = this.repo.create({
       name: book.name,
@@ -18,5 +22,17 @@ export class BooksService {
     });
 
     return this.repo.save(newBook);
+  }
+
+  async findOne(id: string): Promise<Book> {
+    const book = await this.repo.findOne({
+      where: { id },
+    });
+
+    if (!book) {
+      throw new NotFoundException('Book not found.');
+    }
+
+    return book;
   }
 }
